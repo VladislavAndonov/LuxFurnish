eac61763010376ae2bae742f24cf5edbb329f753
Vladislav Andonov <wladoandonov@gmail.com>
2024-07-23 15:00:06 +0300

    Initialize project

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <div className="text-green-400">
+            <h1>React.</h1>
+        </div>
+    );
+}
