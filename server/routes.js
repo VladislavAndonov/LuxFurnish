f31c00c5213d7f51c61f753cf332efd8b05b270f
Vlado <wladoandonov@gmail.com>
2026-03-25 23:31:32 +0200

    Added furniture controller and service

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import authController from "./controllers/authController.js";
+import furnitureController from "./controllers/furnitureController.js";
 
 const routes = Router();
 
 routes.use("/auth", authController);
+routes.use("/furniture", furnitureController);
 
 export default routes
